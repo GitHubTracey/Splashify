51e3cd92ad75bd9b2ec8b569b7993fd6311fbc58
Tracey <traceywindsor@gmail.com>
2016-11-16 10:49:17 -0800

    nav helpers

--- app/components/UserAvatar/UserAvatar.js
+++ app/components/UserAvatar/UserAvatar.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import {
     View,
     ListView,
@@ -6,24 +6,35 @@ import {
     Text,
 } from 'react-native';
 import { avatarStyles } from './styles.js'
-// import _goToPage from '../../lib/navigationHelpers.js'
+// import { _goToPage } from '../../lib/navigationHelpers.js'
+import { withNavigation } from '@exponent/ex-navigation';
 
-const UserAvatar = (props) => {
-    return (
-        <View style={avatarStyles.container} >
-            <View style={avatarStyles.user} >
-                <Image
-                    source={{ uri: props.user.profile_image.large }}
-                    resizeMode={'contain'}
-                    style={avatarStyles.image}
-                    />
-                <Text style={avatarStyles.text}>{props.user.name}</Text>
+class UserAvatar extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View style={avatarStyles.container} >
+                <View onPress={this._goToPage(this.props.navigator, 'user')} style={avatarStyles.user} >
+                    <Image
+                        source={{ uri: props.user.profile_image.large }}
+                        resizeMode={'contain'}
+                        style={avatarStyles.image}
+                        />
+                    <Text style={avatarStyles.text}>{props.user.name}</Text>
+                </View>
             </View>
-        </View>
-    )
+        )
+    }
 }
 
 UserAvatar.propTypes = {
-    user: React.PropTypes.object.isRequired
+    user: React.PropTypes.object.isRequired,
+    route: PropTypes.object.isRequired,
+    navigation: PropTypes.object.isRequired,
+    navigator: PropTypes.object.isRequired,
 }
 export default UserAvatar;
--- app/lib/navigationHelpers.js
+++ app/lib/navigationHelpers.js
@@ -1,10 +1,10 @@
 import Router from '../navigation/routes.js'
 
-_goToPage = (navigator, route) => {
-    navigator.push(Router.getRoute(route));
+export const _goToUser = (navigator, username) => () => {
+    navigator.push(Router.getRoute(username));
 }
 
-_goBack = (navigator) => {
-    if (navigator.getCurrentIndex() > 0)
-        navigator.pop();
-}
+// export const _goBack = (navigator) => {
+//     if (navigator.getCurrentIndex() > 0)
+//         navigator.pop();
+// }
