e28122679f4c5e65ebb426fdba55a8d4ae086752
Tracey <traceywindsor@gmail.com>
2016-11-16 11:42:14 -0800

    add Fave to Realm w/ databaseHelpers

---
+++ app/lib/databaseHelpers.js
@@ -0,0 +1,32 @@
+import Realm from '../config/models.js';
+
+/*
+    A function that checks if a particular photo has been faved. It should return true or false. 
+    We can use it to conditionally render the colour of the star icon in the Photobox scene.
+*/
+export const isPhotoFaved = (photoId) => {
+    let photo = realm.objects('Fave').filtered('id==$0', photoId)
+
+    if (photo.length > 0) {
+        return true;
+    }
+    return false;
+}
+/*
+    A function that saves or deletes a photo as a fave. If the photo is already faved it should be 
+    deleted from the database, otherwise it should be added to the database. We can use this 
+    function to take the appropriate write action when the star icon is tapped in the Photobox scene.
+*/
+export const updatePhotoFave = (photoId, isPhotoFaved) => {
+    if (isPhotoFaved) {
+        let photo = realm.objects('Fave').filtered('id==$0', photoId)
+
+        realm.write(() => {
+            realm.delete(photo)
+        })
+    } else {
+        realm.write(() => {
+            realm.create('Fave', { id: photoId, faved_on: new Date() })
+        })
+    }
+}
