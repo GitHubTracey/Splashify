dcf72638b4e99fd1ac570aad1da4c248f6a13568
Tracey <traceywindsor@gmail.com>
2016-11-20 00:44:21 -0800

    add Faves Listener

--- app/scenes/Favourites/FavouritesContainer.js
+++ app/scenes/Favourites/FavouritesContainer.js
@@ -1,12 +1,13 @@
 import React, { Component, PropTypes, } from 'react';
-import { View, ListView, } from 'react-native';
+import { View, ListView, Text, } from 'react-native';
 import Favourites from './Favourites'
 import Loader from '../../components/Loader'
 import { getFavedPhotos } from '../../lib/databaseHelpers.js'
 import { toJson } from 'unsplash-js/native'
 import { unsplash } from '../../config/apikeys.js'
 import { getFullPhotoData } from '../../lib/unsplashHelpers.js'
-import realm from '../../config/models.js'
+import realm, {Faves} from '../../config/models.js'
+import {styles} from './styles.js'
 
 class FavouritesContainer extends Component {
 
@@ -33,23 +34,25 @@ class FavouritesContainer extends Component {
             isLoading: true,
         };
 
-        // console.log('realm.objects(Fave)', realm.objects('Fave'))
-        // Observe Collection Notifications
-        // realm.objects('Fave').addListener((favourites, changes) => {
-        //     console.log('change!', favourites, changes)
-        //     // Update UI in response to inserted objects
-        //     // changes.insertions.forEach((index) => {
-        //     //     let insertedFavourite = favourites[index];
-        //     //     return true
-        //     // });
+        realm.addListener('change', (changes) => {
+            console.log("Realm Changed...!");
+            console.log(changes)
+            this.getFavedPhotosJson()
+        });
 
-        //     // Update UI in response to deleted objects
-        //     // changes.deletions.forEach((index) => {
-        //         // Deleted objects cannot be accessed directly
-        //         // Support for accessing deleted objects coming soon...
-        //         //...
-        //     //     return false
-        //     // });
+        // realm.objects('Faves').addListener((collection, changes) => {
+        //     changes.insertions.forEach((index) => {
+        //     console.log("Fave added!");
+        //         this.setState({
+        //             dataSource: this.ds.cloneWithRows(fullJsonResults)
+        //         })
+        //     });
+        //     changes.deletions.forEach((index) => {
+        //         console.log("Fave removed")
+        //         this.setState({
+        //             dataSource: this.ds.cloneWithRows(fullJsonResults)
+        //         })
+        //     });
         // });
     }
 
@@ -75,6 +78,10 @@ class FavouritesContainer extends Component {
             });
         }
     }
+    componentWillUnmount() {
+        realm.removeListener()
+        //realm.objects('Fave').removeListener()
+    }
 
     render() {
         if (this.state.isLoading) {
@@ -82,9 +89,17 @@ class FavouritesContainer extends Component {
                 <Loader />
             );
         } else {
-            return (
-                <Favourites photoBlob={this.state.dataSource} user={this.state.user} nav={this.props.navigator} mainNav={this.props.navigation.getNavigator('mainStack')} />
-            );
+            if( this.state.dataSource.length > 0 ) {
+                return (
+                    <Favourites photoBlob={this.state.dataSource} user={this.state.user} nav={this.props.navigator} mainNav={this.props.navigation.getNavigator('mainStack')} />
+                );
+            } else {
+                return (
+                    <View style={styles.container}>
+                        <Text style={styles.text}>You haven't faved any photos yet.</Text>
+                    </View>
+                )
+            }
         }
     }
 }
--- app/scenes/Favourites/styles.js
+++ app/scenes/Favourites/styles.js
@@ -5,7 +5,12 @@ import {
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection: 'column',
         justifyContent: 'center', 
         alignItems: 'center',
+    },
+    text: {
+        flex: 1,
+        fontSize: 16,
     },
 })
