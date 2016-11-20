2a59cab72729dffd6b5ed921159562e744d4392e
Tracey <traceywindsor@gmail.com>
2016-11-19 16:51:32 -0800

    Update Recent to use PhotoWithStats comp

--- app/scenes/Recent/styles.js
+++ app/scenes/Recent/styles.js
@@ -13,5 +13,14 @@ export const styles = StyleSheet.create({
         bottom: 0,
         flexDirection: 'row',
         flex: 1,
-    }
+    },
+    separator: {
+        flex: 1,
+        marginTop: 2,
+        marginRight: 10,
+        marginBottom: 2,
+        marginLeft: 10,
+        height: StyleSheet.hairlineWidth,
+        backgroundColor: '#999999',
+    },
 });
